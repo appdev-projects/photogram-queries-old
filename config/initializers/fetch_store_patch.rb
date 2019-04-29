a1a9159ff116b45a1d81bbfd6f6f53d3e5fa289f
Raghu Betina <raghu@firstdraft.com>
2019-04-29 09:52:47 -0500

    Starting point

---
+++ config/initializers/fetch_store_patch.rb
@@ -0,0 +1,16 @@
+    
+module ActionDispatch
+  class Request
+    class Session
+      alias fetch []
+      alias store []=
+    end
+  end
+
+  class Cookies
+    class CookieJar
+      alias fetch []
+      alias store []=
+    end
+  end
+end
